0697b2b6b5b74c58b1009995a5d96d54420489a2
iritikraj <ritik.raj@sarvm.ai>
2023-11-30 21:37:23 +0530

    Chat Page and Notification page built

--- empowerschool-testapp/src/app/pages/chat/chat.page.ts
+++ empowerschool-testapp/src/app/pages/chat/chat.page.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
+  public names = [
+    'Punit Sir',
+    'Rahul Sir',
+    'Sunita Maam',
+    'Anita Maam',
+    'Vikas Sir',
+    'Deepika Maam',
+    'Amit Sir',
+    'Rajesh Sir',
+    'Shilpa Maam',
+    'Neeta Maam',
+    'Arun Sir',
+    'Kavita Maam',
+    'Vishal Sir',
+  ];
+  items: { name: string; unread: boolean }[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.addItemToTop(3);
   }
 
+  chooseRandomName(): string {
+    return this.names[Math.floor(Math.random() * this.names.length)];
+  }
+
+  addItemToTop = (count: any, unread = false) => {
+    const newItems = [];
+    for (let i = 0; i < count; i++) {
+      newItems.push({
+        name: this.chooseRandomName(),
+        unread,
+      });
+    }
+
+    this.items = newItems.concat(this.items);
+  }
+
+  handleRefresh = (event: any) => {
+    setTimeout(() => {
+      this.addItemToTop(3, true);
+      event.target.complete();
+    }, 2000);
+  };
 }
--- empowerschool-testapp/src/app/pages/notifications/notifications.page.ts
+++ empowerschool-testapp/src/app/pages/notifications/notifications.page.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
+  notifications = [
+    "New message received from teacher",
+    "Your leave request has been accepted",
+    "Event reminder: Class at 2 PM",
+    "Congratulations! You've earned a badge"
+  ]
 
   constructor() { }
 
